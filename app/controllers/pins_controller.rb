a2b4f0fac95ee758f22d5d47391943e3940d0ce3
Allan Sechrist <abyssalspecter@gmail.com>
2014-01-04 13:03:45 -0800

    made changes to pin controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -23,10 +23,10 @@ class PinsController < ApplicationController
     respond_to do |format|
       if @pin.save
         format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @pin }
+        
       else
         format.html { render action: 'new' }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
+        
       end
     end
   end
@@ -35,10 +35,10 @@ class PinsController < ApplicationController
     respond_to do |format|
       if @pin.update(pin_params)
         format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
-        format.json { head :no_content }
+       
       else
         format.html { render action: 'edit' }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
+        
       end
     end
   end
@@ -47,7 +47,7 @@ class PinsController < ApplicationController
     @pin.destroy
     respond_to do |format|
       format.html { redirect_to pins_url }
-      format.json { head :no_content }
+     
     end
   end
 
